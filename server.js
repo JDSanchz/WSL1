664d70d4ca4e4d4648e991179753a1e2da8d0cdd
JD Sanchez <67755598+JDSanchz@users.noreply.github.com>
2023-04-24 16:59:48 -0600

    name

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Jesus Del Barrio | 2023');
+  res.send('Natanael Barch | 2023');
 });
 
 app.listen(port, () => {
