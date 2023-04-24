7e9fca0e9736618a29f1183175913e1e8b7fec99
JD Sanchez <67755598+JDSanchz@users.noreply.github.com>
2023-04-24 17:03:56 -0600

    changes

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Natanael Barch | 2023');
-});
+app.use('/', require('./routes'))
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+    console.log(`Running on port ${port}`)
+})
